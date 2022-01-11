981dcf52b5c86519f076964f16cb5cd68930e63e
sgtanvi <54735473+sgtanvi@users.noreply.github.com>
2022-01-10 22:38:15 -0800

    Delete hello2.java

--- hello2.java
+++
@@ -1,8 +0,0 @@
-/*
- * First Java program to say Hello
- */
-public class hello2 {   // Save as "Hello.java" under "d:\myProject"
-   public static void main(String[] args) {
-      System.out.println("Hello, world!");
-   }
-}
